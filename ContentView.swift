8d4093f6361627da9d3409173cf6dd08ddfb8a22
fandrade8 <f.andrade.8.10@gmail.com>
2020-03-23 20:17:15 -0700

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,67 @@
+//
+//  ContentView.swift
+//  midterm SwiftUI
+//
+//  Created by CSUFTitan on 3/23/20.
+//  Copyright © 2020 CSUFTitan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var tapCount: Int
+    
+    var body: some View {
+//--------------Question 1-------------------------
+//        Text("Text to the right!!!!")
+//            .padding()
+//            .background(Color.red)
+//            .padding()
+//            .background(Color.blue)
+//            .padding()
+//            .padding()
+//            .background(Color.green)
+//-------------------------------------------------
+        
+        
+//--------------Question 2-------------------------
+//        Button("Live Long and Prosper")
+//            .frame(width: 200, height: 200)
+//            .background(Color.red)
+//-------------------------------------------------
+        
+//--------------Question 3-------------------------
+//        NavigationBar {
+//            Form {
+//                Section {
+//                    Text("Hello, world!")
+//                    Text("Hello, world!")
+//                }
+//                Section {
+//                    Text("Hello, World!")
+//                }.navigationBarTitle("SwiftUI")
+//            }
+//        }
+        
+//-------------------------------------------------
+        
+        
+//--------------Question 4-------------------------
+//        Button(action: {self.tapCount += 1}){
+//            Text("Tap count: \(self.tapCount)")
+//                .padding()
+//                .frame(width: 100, height: 100)
+//                .background(Color.blue)
+//                .foregroundColor(.white)
+//                .multilineTextAlignment(.center)
+//        }
+        
+//-------------------------------------------------
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
